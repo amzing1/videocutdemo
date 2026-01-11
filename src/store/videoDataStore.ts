ed52d002e7a06df00e07e46915ef68bd0cca4a15
wuqinghua <wuqinghua@heywhale.com>
2026-01-11 14:28:51 +0800

    feat: add low_bit to compute

--- src/store/videoDataStore.ts
+++ src/store/videoDataStore.ts
@@ -6,7 +6,7 @@ export const useVideoDataStore = defineStore("videoDataStore", () => {
     "https://cdn.kesci.com/admin/t8mzcj10mk/1_processed.mp4",
     "https://cdn.kesci.com/admin/t8mzcj10mk/5_processed.mp4",
     "https://cdn.kesci.com/admin/t8mzcj10mk/3_processed.mp4",
-    // "https://cdn.kesci.com/admin/t8orv3i4b/3_low_bitrate.mp4",
+    "https://cdn.kesci.com/admin/t8orv3i4b/3_low_bitrate.mp4",
     "https://cdn.kesci.com/admin/t8mzcj10mk/4_processed.mp4",
     "https://cdn.kesci.com/admin/t8mzcj10mk/7_processed.mp4",
     "https://cdn.kesci.com/admin/t8mzcj10mk/8_processed.mp4",
