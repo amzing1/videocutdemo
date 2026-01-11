dafc04bc8c422419a07d47c077c5e80d3c6ca27f
wuqinghua <wuqinghua@heywhale.com>
2026-01-11 14:06:59 +0800

    feat: 尝试对某个视频进行HLS并降低码率

--- src/store/videoDataStore.ts
+++ src/store/videoDataStore.ts
@@ -5,7 +5,8 @@ export const useVideoDataStore = defineStore("videoDataStore", () => {
   const videoUrls = ref([
     "https://cdn.kesci.com/admin/t8mzcj10mk/1_processed.mp4",
     "https://cdn.kesci.com/admin/t8mzcj10mk/5_processed.mp4",
-    "https://cdn.kesci.com/admin/t8mzcj10mk/3_processed.mp4",
+    // "https://cdn.kesci.com/admin/t8oqtrron/3.m3u8", // 直接转未降低码率
+    "https://cdn.kesci.com/admin/t8oreay9r/3.m3u8", // 降低码率 ffmpeg -i 3.mp4 -c:v libx264 -g 60 -sc_threshold 0 -b:v 2000k -maxrate 2000k -bufsize 4000k -hls_time 2 -hls_list_size 0 3.m3u8
     "https://cdn.kesci.com/admin/t8mzcj10mk/4_processed.mp4",
     "https://cdn.kesci.com/admin/t8mzcj10mk/7_processed.mp4",
     "https://cdn.kesci.com/admin/t8mzcj10mk/8_processed.mp4",
