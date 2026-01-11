f38784c481d055b608978b1fc6d9305ad785edf0
wuqinghua <wuqinghua@heywhale.com>
2026-01-12 00:00:25 +0800

    feat: all video is m3u8

--- src/store/videoDataStore.ts
+++ src/store/videoDataStore.ts
@@ -3,15 +3,16 @@ import { computed, ref } from "vue";
 
 export const useVideoDataStore = defineStore("videoDataStore", () => {
   const videoUrls = ref([
-    "https://cdn.kesci.com/admin/t8mzcj10mk/1_processed.mp4",
-    "https://cdn.kesci.com/admin/t8mzcj10mk/5_processed.mp4",
-    // "https://cdn.kesci.com/admin/t8oqtrron/3.m3u8", // 直接转未降低码率
-    "https://cdn.kesci.com/admin/t8oreay9r/3.m3u8", // 降低码率 ffmpeg -i 3.mp4 -c:v libx264 -g 60 -sc_threshold 0 -b:v 2000k -maxrate 2000k -bufsize 4000k -hls_time 2 -hls_list_size 0 3.m3u8
-    "https://cdn.kesci.com/admin/t8mzcj10mk/4_processed.mp4",
-    "https://cdn.kesci.com/admin/t8mzcj10mk/7_processed.mp4",
-    "https://cdn.kesci.com/admin/t8mzcj10mk/8_processed.mp4",
-    "https://cdn.kesci.com/admin/t8mzcj10mk/9_processed.mp4",
-    "https://cdn.kesci.com/admin/t8mzcj10mk/10_processed.mp4",
+    "https://cdn.kesci.com/admin/t8piq7gl1/1.m3u8",
+    "https://cdn.kesci.com/admin/t8pis4dk0/2.m3u8",
+    "https://cdn.kesci.com/admin/t8pito7cd/3.m3u8",
+    "https://cdn.kesci.com/admin/t8piu61bjc/4.m3u8",
+    "https://cdn.kesci.com/admin/t8piugo1o/5.m3u8",
+    "https://cdn.kesci.com/admin/t8piuspl9/6.m3u8",
+    "https://cdn.kesci.com/admin/t8piv1hzw/7.m3u8",
+    "https://cdn.kesci.com/admin/t8pivaz1u/8.m3u8",
+    "https://cdn.kesci.com/admin/t8pivlqe3/9.m3u8",
+    "https://cdn.kesci.com/admin/t8pivx7cd/10.m3u8",
   ]);
 
   const currentVideoIdx = ref(0);
